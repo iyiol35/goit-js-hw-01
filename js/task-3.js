a7df6c1a67ffdb1fadd20cefb86d08460768efbf
iyiol35 <iolcar2009@hotmail.com>
2025-01-11 13:42:21 +0300

    js-hw1

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,10 @@
 function getElementWidth(content, padding, border) {
-    return `${content} + ${padding} * 2 + ${border} * 2`;
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding) * 2;
+    const borderWidth = parseFloat(border) * 2;
+    const totalWidth = contentWidth + paddingWidth + borderWidth;
+
+    return totalWidth;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
